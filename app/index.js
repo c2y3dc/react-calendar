e2be2bb96baec0c3bfafcc8163e66d60d645bef2
Ervin Chow <ervinchow@gmail.com>
2016-04-14 14:24:19 -0700

    adds webpack, babel configs and base files

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import routes from './config/routes'
+
+ReactDOM.render( 
+	<h1>"Hello! from React"</h1> , document.getElementById('app')
+);
